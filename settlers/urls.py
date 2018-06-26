fbc87dc70186982e9a2654482ba0352787963751
Katarina Barnhart <barnkaetzchen@gmail.com>
2018-06-25 22:41:59 -0400

    Initial game app commit

--- settlers/urls.py
+++ settlers/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('game/', include('game.urls')),
+    path('', RedirectView.as_view(url='/game/', permanent=True)),
+    path('accounts/', include('django.contrib.auth.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
